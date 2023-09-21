32455f62fc96dec81bd3f6fcf34224e0aeaf8189
Pat Needham <patrick.h.needham@gmail.com>
2023-09-21 08:25:41 -0400

    shadcn added all ui primitives from radix

---
+++ lib/components/ui/index.ts
@@ -0,0 +1,196 @@
+export {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from './accordion.tsx';
+export {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogAction,
+  AlertDialogCancel,
+} from './alert-dialog.tsx';
+export { Alert, AlertTitle, AlertDescription } from './alert.tsx';
+export { AspectRatio } from './aspect-ratio.tsx';
+export { Avatar, AvatarImage, AvatarFallback } from './avatar.tsx';
+export { Badge, badgeVariants } from './badge.tsx';
+export { Button, buttonVariants, type ButtonProps } from './button.tsx';
+export { Calendar, type CalendarProps } from './calendar.tsx';
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from './card.tsx';
+export { Checkbox, type CheckboxProps } from './checkbox.tsx';
+export {
+  Collapsible,
+  CollapsibleTrigger,
+  CollapsibleContent,
+} from './collapsible.tsx';
+export {
+  Command,
+  CommandDialog,
+  CommandInput,
+  CommandList,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+  CommandShortcut,
+  CommandSeparator,
+} from './command.tsx';
+export {
+  ContextMenu,
+  ContextMenuTrigger,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuCheckboxItem,
+  ContextMenuRadioItem,
+  ContextMenuLabel,
+  ContextMenuSeparator,
+  ContextMenuShortcut,
+  ContextMenuGroup,
+  ContextMenuPortal,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuRadioGroup,
+} from './context-menu.tsx';
+export {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+} from './dialog.tsx';
+export {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuGroup,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuRadioGroup,
+} from './dropdown-menu.tsx';
+export {
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+} from './form.tsx';
+export {
+  HoverCard,
+  HoverCardTrigger,
+  HoverCardContent,
+} from './hover-card.tsx';
+export { Input, type InputProps } from './input.tsx';
+export { Label } from './label.tsx';
+export {
+  Menubar,
+  MenubarMenu,
+  MenubarTrigger,
+  MenubarContent,
+  MenubarItem,
+  MenubarSeparator,
+  MenubarLabel,
+  MenubarCheckboxItem,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarPortal,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarGroup,
+  MenubarSub,
+  MenubarShortcut,
+} from './menubar.tsx';
+export {
+  navigationMenuTriggerStyle,
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuContent,
+  NavigationMenuTrigger,
+  NavigationMenuLink,
+  NavigationMenuIndicator,
+  NavigationMenuViewport,
+} from './navigation-menu.tsx';
+export { Popover, PopoverTrigger, PopoverContent } from './popover.tsx';
+export { Progress } from './progress.tsx';
+export { RadioGroup, RadioGroupItem } from './radio-group.tsx';
+export { ScrollArea, ScrollBar } from './scroll-area.tsx';
+export {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+} from './select.tsx';
+export { Separator } from './separator.tsx';
+export {
+  Sheet,
+  SheetTrigger,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetFooter,
+  SheetTitle,
+  SheetDescription,
+} from './sheet.tsx';
+export { Skeleton } from './skeleton.tsx';
+export { Slider } from './slider.tsx';
+export { Switch } from './switch.tsx';
+export {
+  Table,
+  TableHeader,
+  TableBody,
+  TableFooter,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableCaption,
+} from './table.tsx';
+export { Tabs, TabsList, TabsTrigger, TabsContent } from './tabs.tsx';
+export { Textarea } from './textarea.tsx';
+export {
+  type ToastProps,
+  type ToastActionElement,
+  ToastProvider,
+  ToastViewport,
+  Toast,
+  ToastTitle,
+  ToastDescription,
+  ToastClose,
+  ToastAction,
+} from './toast.tsx';
+export { Toaster } from './toaster.tsx';
+export { Toggle, toggleVariants } from './toggle.tsx';
+export {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from './tooltip.tsx';
